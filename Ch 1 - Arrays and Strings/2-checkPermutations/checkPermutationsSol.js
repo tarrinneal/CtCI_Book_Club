bef80441af77f8009476598c8a3769b55b5826d8
tarrinneal <tarrinneal@gmail.com>
2021-04-14 17:58:23 -0700

    minor typo

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutationsSol.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutationsSol.js
@@ -1,5 +1,5 @@
-const checkPermute = (str1, str2) => {
+const checkPermutations = (str1, str2) => {
   return str1.split('').sort().join('') === str2.split('').sort().join('');
 };
 
-module.exports = checkPermute;
+module.exports = checkPermutations;
