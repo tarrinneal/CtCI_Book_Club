4d6fe0b44264cd3934cbed18ed0682beb96be4f5
Parker Stafford <parker.stafford92@gmail.com>
2021-04-15 16:29:06 -0700

    change to toStrictEqual from toBe

--- Ch 2 - Linked Lists/1-removeDups/removeDups.test.js
+++ Ch 2 - Linked Lists/1-removeDups/removeDups.test.js
@@ -1,6 +1,6 @@
 const removeDups = require('./removeDups');
 const removeDupsSol = require('./removeDupsSol');
-const llGenerator = require('../llGenerator');
+const { llGenerator } = require('../llHelpers');
 
 const noDupsLL = llGenerator([1, 5, 2, 3, 11, 7]);
 const noDupsLL2 = llGenerator([400, 22, 390, 1, 57])
@@ -10,16 +10,16 @@ const nonConsecDupsLL = llGenerator([2, 5, 2, 13, 7, 8, 4, 13, 4, 5, 4]);
 const nonConsecDupsLL2 = llGenerator([45, 29, 78, 45, 2, 3, 99, 3, 21])
 
 test('does not modify a linked list with no dups', () => {
-  expect(removeDups(noDupsLL)).toBe(noDupsLL);
-  expect(removeDups(noDupsLL2)).toBe(noDupsLL2);
+  expect(removeDups(noDupsLL)).toStrictEqual(noDupsLL);
+  expect(removeDups(noDupsLL2)).toStrictEqual(noDupsLL2);
 });
 
 test('removes consecutive duplicates in a linked list', () => {
-  expect(removeDups(consecDupsLL)).toBe(removeDupsSol(consecDupsLL));
-  expect(removeDups(consecDupsLL2)).toBe(removeDupsSol(consecDupsLL2));
+  expect(removeDups(consecDupsLL)).toStrictEqual(removeDupsSol(consecDupsLL));
+  expect(removeDups(consecDupsLL2)).toStrictEqual(removeDupsSol(consecDupsLL2));
 });
 
 test('removes non consecutive duplicates from a linked list', () => {
-  expect(removeDups(nonConsecDupsLL)).toBe(removeDupsSol(nonConsecDupsLL));
-  expect(removeDups(nonConsecDupsLL2)).toBe(removeDupsSol(nonConsecDupsLL2));
+  expect(removeDups(nonConsecDupsLL)).toStrictEqual(removeDupsSol(nonConsecDupsLL));
+  expect(removeDups(nonConsecDupsLL2)).toStrictEqual(removeDupsSol(nonConsecDupsLL2));
 })
