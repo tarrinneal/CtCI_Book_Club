8083d8b63ff6a2e1ff8c1bebf8671cc585b20743
larakhdavies <lara.kh.davies@gmail.com>
2021-04-16 06:31:37 -1000

    question1 and 2

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,7 +1,28 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
-function isUnique(str) {
-  //code
-}
+// function isUnique(str) {
+//   let hash = new Set();
+//   for (let i = 0; i<str.length-1; i++){
+//     if(hash.has(str[i])){
+//       return false
+//     } else {
+//       hash.add(str[i])
+//     }
+//   }
+//   return true
+// }
+//liner time - using a hash data structure
+//space complexity O(n)
 
+function isUnique(str) {
+  let objOfChars = {};
+  for (let i = 0; i<str.length; i++) {
+    if(objOfChars[str[i]]){
+      return false
+    } else {
+      objOfChars[str[i]] = 1;
+    }
+  }
+  return true;
+}
 module.exports = isUnique;
--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,7 +1,15 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
 
 const checkPermutations = (str1, str2) => {
-  //Please code here
+  if (string1.length !== string2.length){
+    return false
+}
+let sortedString1 = string1.split('').sort().join('');
+let sortedString2 = string2.split('').sort().join('');
+if(sortedString1 === sortedString2){
+    return true;
+}
+return false;
 };
 
 module.exports = checkPermutations;
