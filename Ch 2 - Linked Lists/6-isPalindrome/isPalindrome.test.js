a15ed3ba2cbec4f2e7da0519efbe330da1506cc6
Parker Stafford <parker.stafford92@gmail.com>
2021-04-19 19:57:03 -0700

    change tests to test submission not solution function

--- Ch 2 - Linked Lists/6-isPalindrome/isPalindrome.test.js
+++ Ch 2 - Linked Lists/6-isPalindrome/isPalindrome.test.js
@@ -6,16 +6,16 @@ const { llGenerator } = require('../llHelpers');
 
 test('returns false for non palindromes', () => {
   const ll = llGenerator([1, 2, 3, 4, 5, 6])
-  expect(isPalindromeSol(ll)).toBe(false);
+  expect(isPalindrome(ll)).toBe(false);
 });
 
 test('returns true for even length palindromes', () => {
   const ll = llGenerator([1, 2, 3, 3, 2, 1])
-  expect(isPalindromeSol(ll)).toBe(true);
+  expect(isPalindrome(ll)).toBe(true);
 });
 
 test('returns true for odd length palindromes', () => {
   const ll = llGenerator([1, 2, 3, 2, 1])
-  expect(isPalindromeSol(ll)).toBe(true);
+  expect(isPalindrome(ll)).toBe(true);
 });
 
