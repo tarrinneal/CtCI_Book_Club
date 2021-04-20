0b8af0964c020fd45218416c692feca3ea8cb198
Parker Stafford <parker.stafford92@gmail.com>
2021-04-19 19:42:30 -0700

    add problem description and function skeleton

---
+++ Ch 2 - Linked Lists/6-isPalindrome/isPalindrome.js
@@ -0,0 +1,26 @@
+// Write a function to check if a linked list is a palindrome.
+
+/*
+Example:
+ll1 = [1, 2, 3, 4, 5, 6] -> false
+ll2 = [1, 2, 3, 2, 1] -> true
+*/
+
+/*
+Definition of a node in a linked list
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+*/
+function Node(val) {
+  this.val = val;
+  this.next = null;
+}
+
+const isPalindrome = (head) => {
+  //Please code here
+};
+
+
+module.exports = isPalindrome;
