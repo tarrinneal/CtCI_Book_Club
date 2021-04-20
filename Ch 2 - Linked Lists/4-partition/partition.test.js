6bd7208b5592084a60c19793f917e6aeb1f74d02
Parker Stafford <parker.stafford92@gmail.com>
2021-04-19 18:10:24 -0700

    add test linked lists for solutions that modify in place

--- Ch 2 - Linked Lists/4-partition/partition.test.js
+++ Ch 2 - Linked Lists/4-partition/partition.test.js
@@ -5,22 +5,25 @@ const { llGenerator, iterateToNode, partitioner } = require('../llHelpers');
 
 
 test('successfully partitions a linked list with a right and a left side', () => {
-  let ll = llGenerator([22, 31, 1, 5, 17, 21, 21, 6, 2, 1, 20]);
-  let solution = partitioner(partitionSol(ll, 20), 6);
-  let test = partitioner(partition(ll, 20), 6);
-  expect(solution).toStrictEqual(test);
+  const ll = llGenerator([22, 31, 1, 5, 17, 21, 21, 6, 2, 1, 20]);
+  const llTest = llGenerator([22, 31, 1, 5, 17, 21, 21, 6, 2, 1, 20]);
+  let solution = partitioner(partitionSol(llTest, 20), 6);
+  let result = partitioner(partition(ll, 20), 6);
+  expect(result).toStrictEqual(solution);
 });
 
 test('successfully partitions a linked list with only a right side', () => {
-  let ll = llGenerator([22, 31, 21, 21, 20])
-  let solution = partitioner(partitionSol(ll, 20), 0);
-  let test = partitioner(partition(ll, 20), 0);
-  expect(solution).toStrictEqual(test);
+  const ll = llGenerator([22, 31, 21, 21, 20]);
+  const llTest = llGenerator([22, 31, 21, 21, 20]);
+  const solution = partitioner(partitionSol(ll, 20), 0);
+  const result = partitioner(partition(ll, 20), 0);
+  expect(result).toStrictEqual(solution);
 });
 
 test('successfully partitions a linked list with only a left side', () => {
-  let ll = llGenerator([1,5,17,6,2,1])
-  let solution = partitioner(partitionSol(ll, 20), 6);
-  let test = partitioner(partition(ll, 20), 6);
-  expect(solution).toStrictEqual(test);
+  const ll = llGenerator([1,5,17,6,2,1]);
+  const llTest = llGenerator([1,5,17,6,2,1]);
+  const solution = partitioner(partitionSol(llTest, 20), 6);
+  const result = partitioner(partition(ll, 20), 6);
+  expect(result).toStrictEqual(solution);
 });
