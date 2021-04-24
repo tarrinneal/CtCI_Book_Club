1f450f939d6b12052144804a72109ba1aa79d124
tarrinneal <tarrinneal@gmail.com>
2021-04-24 14:30:09 -0700

    check perms

--- Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
+++ Ch 1 - Arrays and Strings/2-checkPermutations/checkPermutations.js
@@ -1,7 +1,7 @@
 //Given two strings, write a method to decide if one is a permutation of the other.
 
 const checkPermutations = (str1, str2) => {
-  //Please code here
+  return str1.split('').sort().join('') === str2.split('').sort().join('');
 };
 
 module.exports = checkPermutations;
