74761dfbff9d06ffe34b71a5a3f9fdb01b20c5b7
tarrinneal <tarrinneal@gmail.com>
2021-04-24 12:01:07 -0700

    added bonus solution to isUnique

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -4,4 +4,16 @@ function isUnique(str) {
   return new Set(str.split('')).size === str.length;
 }
 
+// function isUnique(str) {
+//   //solution with no added data structures
+//   for (let i = 0; i < str.length; i++) {
+//     for (let j = i + 1; j < str.length; j++) {
+//       if (str[i] === str[j]) {
+//         return false;
+//       }
+//     }
+//   }
+//   return true;
+// }
+
 module.exports = isUnique;
