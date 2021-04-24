e64ee74de9cf5fbfadaee82a346e938b9d5ccea3
tarrinneal <tarrinneal@gmail.com>
2021-04-24 11:55:07 -0700

    isUnique one line, no bonus

--- Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
+++ Ch 1 - Arrays and Strings/1-isUnique/isUnique.js
@@ -1,7 +1,7 @@
 //Implement an algorithm to determine if a string has all unique characters. What if you cannot use additional data structures?
 
 function isUnique(str) {
-  //code
+  return new Set(str.split('')).size === str.length;
 }
 
 module.exports = isUnique;
