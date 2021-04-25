e4dcc3ae4d3fd83ac4dfb845cf4cecf0372d4dcb
tarrinneal <tarrinneal@gmail.com>
2021-04-25 11:07:31 -0700

    URLify

--- Ch 1 - Arrays and Strings/3-URLify/URLify.js
+++ Ch 1 - Arrays and Strings/3-URLify/URLify.js
@@ -1,7 +1,13 @@
 //Write a method to replace all spaces in a string with '%20'.
 
 const URLify = (str) => {
-  //You write code here
+  let arr = str.split('');
+  for (let i = arr.length - 1; i >= 0; i--) {
+    if (arr[i] === ' ') {
+      arr[i] = '%20';
+    }
+  }
+  return arr.join('');
 };
 
 module.exports = URLify;
