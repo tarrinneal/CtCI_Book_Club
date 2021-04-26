f21088c36ba4f39a9748e2f826d48b218aa90283
tarrinneal <tarrinneal@gmail.com>
2021-04-26 00:08:03 -0700

    zeroMatrix

--- Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
+++ Ch 1 - Arrays and Strings/8-zeroMatrix/zeroMatrix.js
@@ -1,7 +1,31 @@
 //Write an algorithm such that if an element in an M x N matrix is 0, its entire row and column are set to 0.
 
 function zeroMatrix(matrix) {
-  //The void goes here
+  let rows = {};
+  let cols = {};
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (matrix[i][j] === 0) {
+        rows[i] = 1;
+        cols[j] = 1;
+      }
+    }
+  }
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (rows[i]) {
+      for (let j = 0; j < matrix[i].length; j++) {
+        matrix[i][j] = 0;
+      }
+    }
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (cols[j]) {
+        matrix[i][j] = 0;
+      }
+    }
+  }
+
+  return matrix;
 }
 
 module.exports = zeroMatrix;
