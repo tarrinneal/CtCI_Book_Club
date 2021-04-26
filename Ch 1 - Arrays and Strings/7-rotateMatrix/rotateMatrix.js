c4f2ceb0d9c075f97602ed8142e42db0689e696a
tarrinneal <tarrinneal@gmail.com>
2021-04-25 22:58:15 -0700

    rotateMatrix

--- Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
+++ Ch 1 - Arrays and Strings/7-rotateMatrix/rotateMatrix.js
@@ -1,7 +1,16 @@
 //Given an image represented by an N x N matrix, where each pixel in the image is represented by an integer, write a method to rotate the image by 90 degrees. Can you do this in place?
 
 function rotateMatrix(matrix) {
-  //EVERYTHING MUST ROTATE!
+  let newMatrix = [];
+  for (let i = matrix.length - 1; i >= 0; i--) {
+    for (let j = 0; j < matrix[i].length; j++) {
+      if (newMatrix[j] === undefined) {
+        newMatrix[j] = [];
+      }
+      newMatrix[j][matrix.length - 1 - i] = matrix[i][j];
+    }
+  }
+  return newMatrix;
 }
 
 module.exports = rotateMatrix;
