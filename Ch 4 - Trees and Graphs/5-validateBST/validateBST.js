acd18b2de8c24bcd9d0b56da1a039e98416fe2c2
tarrinneal <tarrinneal@gmail.com>
2021-05-09 12:03:52 -0700

    fixed description of validateBST

--- Ch 4 - Trees and Graphs/5-validateBST/validateBST.js
+++ Ch 4 - Trees and Graphs/5-validateBST/validateBST.js
@@ -1,4 +1,4 @@
-//Given a binary tree, design an algorithm which creates a linked list of all the nodes at each depth (e.g., if you have a tree with depth D, you'll have D linked lists).
+//Implement a function to check if a binary tree is a binary search tree.
 
 var BinaryTree = function (value) {
   this.value = value;
