435803bc3ce5645c3b83b776a1d4d0dac3a42440
tarrinneal <tarrinneal@gmail.com>
2021-05-16 18:16:22 -0700

    flipBitToWin, and fix for typo in binaryToString

--- Ch 5 - Bit Manipulation/2-binaryToString/binaryToString.test.js
+++ Ch 5 - Bit Manipulation/2-binaryToString/binaryToString.test.js
@@ -1,9 +1,6 @@
 const binaryToString = require('./binaryToString');
 const binaryToStringSol = require('./binaryToStringSol');
 
-const N = parseInt(10000000000, 2);
-const M = parseInt(10011, 2);
-
 test('Basic binaryToString test', () => {
   expect(binaryToString(0.625)).toBe('0.101');
   expect(binaryToString(0.6255342856783467856932)).toBe('ERROR');
